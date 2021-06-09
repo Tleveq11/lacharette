bab457d0522a97c00db1c19fc69d1af42036e268
tanguy leveque <leveque.tanguy@outlook.fr>
2021-06-09 20:38:24 +0200

    index pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  root to: 'pages#home'
+  root to: 'pages#index'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+get 'home', to: 'pages#home'
+get 'index', to: 'pages#index'
 get 'services', to: 'pages#services'
 get 'equipe', to: 'pages#equipe'
 get 'realisations', to: 'pages#realisations'
